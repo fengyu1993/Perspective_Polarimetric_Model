499ab38844995b10be308e17ed1384ca2fd83d8c
fengyu1993 <chenyuhan19930920@163.com>
2026-07-30 15:41:04 +0800

    Signed-off-by: fengyu1993 <chenyuhan19930920@163.com>

---
+++ Experiment_DeepSfPData.m
@@ -0,0 +1,189 @@
+%% Real Data (DeepSfP Dataset) Complex Surfaces Comparision
+clc; clear; close all;
+%%
+[location, name] = get_name_DeepSfP();
+index = get_DeepSfP_test_name(name);
+%%
+row = 1024; col = 1224;
+V_orth = zeros(row, col, 3); V_orth(:,:,3) = -1;
+Beta_orth = zeros(row, col); 
+mask = ones(row, col);  Mask = mask == 1;
+%%
+eta = 1.5;
+a_list = [0.79, 0.24, 0.11, 0.14, 0.29, 0.12];
+f_xy = 3478;
+K = [-f_xy, 0, 612; 0, f_xy, 512; 0, 0, 1];
+%% 
+Err_pers = NaN(row, col, 6);
+Err_orth = NaN(row, col, 6);
+Mask_id= NaN(row, col, 6);
+%% 
+for caseNum = 1 : length(index.name)
+    a = a_list(caseNum);
+    err = zeros(row, col);
+    err_orth = zeros(row, col);
+    sumMask = zeros(row, col);
+    err_all = zeros(row, col);
+    err_orth_all = zeros(row, col);
+    sumMask_all = zeros(row, col);
+    for flag_case = 1 : 3
+        caseName = index.name{caseNum};
+        rangeIndoorNum = index.indoorNumber{caseNum};
+        rangeOutdoorCloudyNum = index.outdoorCloudyNumber{caseNum};
+        rangeOutdoorSunnyNum = index.outdoorSunnyNumber{caseNum};
+        %% test
+        switch flag_case
+            case 1
+                %% Indoor
+                for i = 1 : length(rangeIndoorNum)             
+                    fprintf('Processing Image Indoor: case = %s, a = %.3f, f  = %.3f, k =  %i/%i ...\n', caseName, a, f_xy, i, length(rangeIndoorNum));
+                    %% Data 
+                    [polarImage, Mask, N_desired] = readDeepSfPData(location.indoor, name.indoor{rangeIndoorNum(i)});
+                    V = getViewingDirection(K, Mask);  
+                    Beta = getPerspectiveDistortionAngle(V, Mask);
+                    N = get_Perspective_SurfaceNormal(polarImage, Beta, V, eta, a, Mask);
+                    N_orth = get_Perspective_SurfaceNormal(polarImage, Beta_orth, V_orth, eta, a, Mask);
+                    %% Error
+                    error_n = getErrorNormalAngle(N, N_desired, Mask);
+                    error_n_orth = getErrorNormalAngle(N_orth, N_desired, Mask);
+                    %% Statistics
+                    err(Mask) = err(Mask) + error_n(Mask);
+                    err_orth(Mask) = err_orth(Mask) + error_n_orth(Mask);
+                    sumMask(Mask) = sumMask(Mask) + 1;
+                    err_all(Mask) = err_all(Mask) + error_n(Mask);
+                    err_orth_all(Mask) = err_orth_all(Mask) + error_n_orth(Mask);
+                    sumMask_all(Mask) = sumMask_all(Mask) + 1;
+                end
+            case 2
+                %% Outdoor Cloudy
+                for i = 1 : length(rangeOutdoorCloudyNum)             
+                    fprintf('Processing Image Outdoor Cloudy: case = %s, a = %.3f, f = %.3f, k =  %i/%i ...\n', caseName, a, f_xy, i, length(rangeOutdoorCloudyNum));
+                    %% Data 
+                    [polarImage, Mask, N_desired] = readDeepSfPData(location.outdoor_cloudy, name.outdoor_cloudy{rangeOutdoorCloudyNum(i)});
+                    V = getViewingDirection(K, Mask);  
+                    Beta = getPerspectiveDistortionAngle(V, Mask);
+                    N = get_Perspective_SurfaceNormal(polarImage, Beta, V, eta, a, Mask);
+                    N_orth = get_Perspective_SurfaceNormal(polarImage, Beta_orth, V_orth, eta, a, Mask);    
+                    %% Error
+                    error_n = getErrorNormalAngle(N, N_desired, Mask);
+                    error_n_orth = getErrorNormalAngle(N_orth, N_desired, Mask);
+                    %% Statistics
+                    err(Mask) = err(Mask) + error_n(Mask);
+                    err_orth(Mask) = err_orth(Mask) + error_n_orth(Mask);
+                    sumMask(Mask) = sumMask(Mask) + 1;
+                    err_all(Mask) = err_all(Mask) + error_n(Mask);
+                    err_orth_all(Mask) = err_orth_all(Mask) + error_n_orth(Mask);
+                    sumMask_all(Mask) = sumMask_all(Mask) + 1;
+                end
+            case 3
+                %% Outdoor Sunny
+                for i = 1 : length(rangeOutdoorSunnyNum)             
+                    fprintf('Processing Image Outdoor Sunny: case = %s, a = %.3f, f = %.3f, k =  %i/%i ...\n', caseName, a, f_xy, i, length(rangeOutdoorSunnyNum));
+                    %% Data 
+                    [polarImage, Mask, N_desired] = readDeepSfPData(location.outdoor_sunny, name.outdoor_sunny{rangeOutdoorSunnyNum(i)});
+                    V = getViewingDirection(K, Mask);  
+                    Beta = getPerspectiveDistortionAngle(V, Mask);
+                    N = get_Perspective_SurfaceNormal(polarImage, Beta, V, eta, a, Mask);
+                    N_orth = get_Perspective_SurfaceNormal(polarImage, Beta_orth, V_orth, eta, a, Mask);
+                    %% Error
+                    error_n = getErrorNormalAngle(N, N_desired, Mask);
+                    error_n_orth = getErrorNormalAngle(N_orth, N_desired, Mask);
+                    %% Statistics
+                    err(Mask) = err(Mask) + error_n(Mask);
+                    err_orth(Mask) = err_orth(Mask) + error_n_orth(Mask);
+                    sumMask(Mask) = sumMask(Mask) + 1;
+                    err_all(Mask) = err_all(Mask) + error_n(Mask);
+                    err_orth_all(Mask) = err_orth_all(Mask) + error_n_orth(Mask);
+                    sumMask_all(Mask) = sumMask_all(Mask) + 1;
+                end
+        end
+    end
+    %% Statistics
+    id = sumMask > 0;
+    err_mean = err ./ sumMask;
+    err_orth_mean = err_orth ./ sumMask;
+    %% Record
+    Err_pers(:,:, caseNum) = err_mean;
+    Err_orth(:,:, caseNum) = err_orth_mean;
+    Mask_id(:,:, caseNum) = id;    
+end
+%% Statistics
+Mask_id_all = sumMask_all > 0;
+Err_pers_all = err_all(Mask_id_all) ./ sumMask_all(Mask_id_all);
+Err_orth_all = err_orth_all(Mask_id_all) ./ sumMask_all(Mask_id_all); 
+%%
+for caseNum = 1 : length(index.name)
+    err_pers = Err_pers(:,:, caseNum);
+    err_orth = Err_orth(:,:, caseNum);
+    mask = Mask_id(:,:, caseNum) == 1;
+    fprintf('%s: \n \t pers / orth --- %.3f, %.3f\n', index.name{caseNum}, rad2deg(mean(err_pers(mask))), rad2deg(mean(err_orth(mask))));
+end
+fprintf('Whole set: \n \t pers / orth --- %.3f, %.3f\n', rad2deg(mean(err_pers(mask))), rad2deg(mean(err_orth(mask))));
+
+
+
+%%
+function [location, name] = get_name_DeepSfP()
+    location.indoor = './Data/DeepSfPData/SurfaceNormals/objects/indoor/';
+    location.outdoor_cloudy = './Data/DeepSfPData/SurfaceNormals/objects/outdoor_cloudy/';
+    location.outdoor_sunny = './Data/DeepSfPData/SurfaceNormals/objects/outdoor_sunny/';
+    filePattern.indoor = fullfile(location.indoor, '*.mat');
+    filePattern.outdoor_cloudy = fullfile(location.outdoor_cloudy, '*.mat');
+    filePattern.outdoor_sunny = fullfile(location.outdoor_sunny, '*.mat');
+    dirData.indoor = dir(filePattern.indoor);
+    dirData.outdoor_cloudy = dir(filePattern.outdoor_cloudy);
+    dirData.outdoor_sunny = dir(filePattern.outdoor_sunny);
+    name.indoor = {dirData.indoor.name};
+    name.outdoor_cloudy = {dirData.outdoor_cloudy.name};
+    name.outdoor_sunny = {dirData.outdoor_sunny.name};
+end
+
+function [polarImage, Mask, N_desired] = readDeepSfPData(location, name)
+    data = load([location, name]);
+    polarImage.I0 = data.images(:,:,1);
+    polarImage.I45 = data.images(:,:,2);
+    polarImage.I90 = data.images(:,:,3);
+    polarImage.I135 = data.images(:,:,4);
+    Mask = data.mask == 1;
+    N_desired = data.normals_gt;
+    N_desired(:,:,2) = -N_desired(:,:,2);
+    N_desired(:,:,3) = -N_desired(:,:,3);
+end
+
+function index = get_DeepSfP_test_name(name)
+    index.name = ["box"; "dragon"; "father_christmas"; "flamingo"; "horse"; "vase"];
+    index.indoorNumber = cell(length(index.name), 1);
+    index.outdoor_cloudyNumber = cell(length(index.name), 1);
+    index.outdoor_sunnyNumber = cell(length(index.name), 1);
+    for i = 1 : length(index.name)
+        index.indoorNumber{i} = find(startsWith(name.indoor, index.name(i), 'IgnoreCase', true));
+        index.outdoorCloudyNumber{i} = find(startsWith(name.outdoor_cloudy, index.name(i), 'IgnoreCase', true));
+        index.outdoorSunnyNumber{i} = find(startsWith(name.outdoor_sunny, index.name(i), 'IgnoreCase', true));  
+    end
+end
+
+function N = get_Perspective_SurfaceNormal(polarImage, Beta, V, eta, a, Mask)
+    %% spdp
+    Rho = getDoLP(polarImage, Mask);
+    Theta_sp = getZenithAngleSpecularReflection(Rho ./ a, Mask, Beta, eta);
+    Phi = getAzimuthAngleDiffuseReflection(polarImage, Mask);
+    N.sp1dp1 = getSurfaceNormal(V, Theta_sp.sp1, Phi.dp1, Mask);
+    N.sp1dp2 = getSurfaceNormal(V, Theta_sp.sp1, Phi.dp2, Mask);
+    N.sp1dp3 = getSurfaceNormal(V, Theta_sp.sp1, Phi.dp3, Mask);
+    N.sp1dp4 = getSurfaceNormal(V, Theta_sp.sp1, Phi.dp4, Mask);
+    N.sp2dp1 = getSurfaceNormal(V, Theta_sp.sp2, Phi.dp1, Mask);
+    N.sp2dp2 = getSurfaceNormal(V, Theta_sp.sp2, Phi.dp2, Mask);
+    N.sp2dp3 = getSurfaceNormal(V, Theta_sp.sp2, Phi.dp3, Mask);
+    N.sp2dp4 = getSurfaceNormal(V, Theta_sp.sp2, Phi.dp4, Mask); 
+    %% spdp_2
+%     N_1 = getSurfaceNormalFromSpecularReflection(polarImage, Beta, V, eta, a, Mask);
+%     N_2 = getSurfaceNormalFromDiffuseReflection(polarImage, Beta, V, eta, a, Mask);
+%     N.sp1 = N_1.sp1;
+%     N.sp2 = N_1.sp2;
+%     N.sp3 = N_1.sp3;
+%     N.sp4 = N_1.sp4;
+%     N.dp1 = N_2.dp1;
+%     N.dp2 = N_2.dp2;
+%     N.dp3 = N_2.dp3;
+%     N.dp4 = N_2.dp4;
+end
---
+++ Experiment_DeepSfPDataTest_4.m
@@ -0,0 +1,147 @@
+%% Real Data (DeepSfP Dataset) Complex Surfaces Comparision: select parameter
+clc; clear; close all;
+%% 
+location = ['.\Data\', '20260730_Data_DeepSfP_Test\'];
+methodName = ["sp", "dp", "spdp", "spdp_2"];
+objectName = ["box", "dragon", "father_christmas", "flamingo", "horse", "vase"];
+caseName = ["indoor", "outdoor_cloudy", "outdoor_sunny"];
+otherName = ["Data_DeepSfP_dataset_", "test_A_FXY_", ".mat"];
+%% select F_xy
+Err_pers = NaN(3, 6);
+numF = 4;
+aMat = NaN(3, 6);
+for methodNum = 3 : 3%length(methodName)
+    for caseNum = 1 : length(caseName)
+        for objectNum = 1 : length(objectName)
+            fprintf("%s -- %s -- %s\n", caseName(caseNum), objectName(objectNum), methodName(methodNum));
+            data = load([location, char(otherName(1)), char(caseName(caseNum)), '_', char(objectName(objectNum)), '_', char(otherName(2)), char(methodName(methodNum)), char(otherName(3))]);
+            %%
+            [k, col] = min(data.error_all_N_angle(numF, :));
+            aMat(caseNum, objectNum) = col;
+            Err_pers(caseNum, objectNum) = k;
+            A(caseNum, objectNum) = data.A(numF, col);
+            F(caseNum, objectNum) = data.F_XY(numF, col);  
+            Err_orth(caseNum, objectNum) = data.error_all_N_angle_orth(numF, col); 
+        end
+    end
+end
+fprintf("Ours / Orthographic: %.3f, %.3f\n", rad2deg(mean(Err_pers(:))), rad2deg(mean(Err_orth(:))));
+fprintf("Best f_xy: %i\n", F(1,1));
+%% select a
+mean(A)
+%% test
+Err_pers = NaN(3, 6);
+Err_orth = NaN(3, 6);
+A = NaN(3, 6);
+F = NaN(3, 6);
+for methodNum = 3 : 3%length(methodName)
+    for caseNum = 1 : length(caseName)
+        for objectNum = 1 : length(objectName)
+            fprintf("%s -- %s -- %s\n", caseName(caseNum), objectName(objectNum), methodName(methodNum));
+            data = load([location, char(otherName(1)), char(caseName(caseNum)), '_', char(objectName(objectNum)), '_', char(otherName(2)), char(methodName(methodNum)), char(otherName(3))]);
+            %%
+            k = min(data.error_all_N_angle(:));
+            [row, col] = find(data.error_all_N_angle == k);
+            Err_pers(caseNum, objectNum) = k;
+            A(caseNum, objectNum) = data.A(row, col);
+            F(caseNum, objectNum) = data.F_XY(row, col);  
+            Err_orth(caseNum, objectNum) = data.error_all_N_angle_orth(row, col); 
+            %% plot
+            figure; hold on; grid on;
+            mesh(data.A, data.F_XY, rad2deg(data.error_all_N_angle), 'EdgeColor', 'r'); 
+            mesh(data.A, data.F_XY, rad2deg(data.error_all_N_angle_orth), 'EdgeColor', 'g'); 
+            view([30,10]);
+            legend('Ours', 'Orthographic');
+            xlabel('a'); ylabel('f'); zlabel('Mean Error');
+            title(caseName(caseNum) + "  " + objectName(objectNum) + "  " + methodName(methodNum))
+        end
+    end
+end
+fprintf("Ours / Orthographic: %.3f, %.3f\n", rad2deg(mean(Err_pers(:))), rad2deg(mean(Err_orth(:))));
+
+%% Case
+Err_pers = zeros(5, 35);
+Err_orth = zeros(5, 35);
+for methodNum = 3 : 3%length(methodName)
+    for caseNum = 1 : length(caseName)
+        for objectNum = 1 : length(objectName)
+            fprintf("%s -- %s -- %s\n", caseName(caseNum), objectName(objectNum), methodName(methodNum));
+            data = load([location, char(otherName(1)), char(caseName(caseNum)), '_', char(objectName(objectNum)), '_', char(otherName(2)), char(methodName(methodNum)), char(otherName(3))]);
+            Err_pers = Err_pers + data.error_all_N_angle;
+            Err_orth = Err_orth + data.error_all_N_angle_orth;
+        end
+        Err_pers = Err_pers / length(objectName);
+        Err_orth = Err_orth / length(objectName);
+        figure; hold on; grid on;
+        %% plot
+        mesh(data.A, data.F_XY, rad2deg(Err_pers), 'EdgeColor', 'r'); 
+        mesh(data.A, data.F_XY, rad2deg(Err_orth), 'EdgeColor', 'g');
+        view([30,10]);
+        legend('Ours', 'Orthographic');
+        xlabel('a'); ylabel('f'); zlabel('Mean Error');
+        title(caseName(caseNum) + "  " + methodName(methodNum))
+    end
+
+end
+%% Object
+Err_pers = zeros(5, 35);
+Err_orth = zeros(5, 35);
+for methodNum = 3 : 3%length(methodName)
+    for objectNum = 1 : length(objectName)
+        for caseNum = 1 : length(caseName)
+            fprintf("%s -- %s -- %s\n", caseName(caseNum), objectName(objectNum), methodName(methodNum));
+            data = load([location, char(otherName(1)), char(caseName(caseNum)), '_', char(objectName(objectNum)), '_', char(otherName(2)), char(methodName(methodNum)), char(otherName(3))]);
+            Err_pers = Err_pers + data.error_all_N_angle;
+            Err_orth = Err_orth + data.error_all_N_angle_orth;
+        end
+        Err_pers = Err_pers / length(caseName);
+        Err_orth = Err_orth / length(caseName);
+        figure; hold on; grid on;
+        %% plot
+        mesh(data.A, data.F_XY, rad2deg(Err_pers), 'EdgeColor', 'r'); 
+        mesh(data.A, data.F_XY, rad2deg(Err_orth), 'EdgeColor', 'g');
+        view([30,10]);
+        legend('Ours', 'Orthographic');
+        xlabel('a'); ylabel('f'); zlabel('Mean Error');
+        title(objectName(objectNum) + "  " + methodName(methodNum))
+    end
+
+end
+
+
+
+
+
+            k = min(rad2deg(data.error_all_N_angle(:)));
+            [row, col] = find(rad2deg(data.error_all_N_angle) == k);
+            data.A(row, col)
+            data.F_XY(row, col)
+
+
+%% 期望参数
+% 焦距
+f_xy = 3478;
+%% dragon: indoor, outdoor_cloudy, outdoor_sunny
+% spdp2: a = 1;
+% spdp: a = 0.2323 f_xy = 1739; (better)
+%% father_christmas: indoor, outdoor_cloudy, outdoor_sunny
+% spdp: a = 0.1264 f_xy = 1739; 
+%% flamingo: indoor, outdoor_cloudy, outdoor_sunny
+% spdp: a = 0.205 f_xy = 2319; 
+
+
+
+
+
+            %% plot
+%             figure; hold on; grid on;
+%             mesh(data.A, data.F_XY, rad2deg(data.error_all_N_angle), 'EdgeColor', 'r'); 
+%             mesh(data.A, data.F_XY, rad2deg(data.error_all_N_angle_orth), 'EdgeColor', 'g'); 
+%             view([30,10]);
+%             legend('Ours', 'Orthographic');
+%             xlabel('a'); ylabel('f'); zlabel('Mean Error');
+%             title(caseName(caseNum) + "  " + objectName(objectNum) + "  " + methodName(methodNum))
+
+
+
+
